6b634d1aa865290a00ed4a98e0a0d2ee42ba8031
Sergey Sharov <Kreozot@users.noreply.github.com>
2026-08-30 22:59:33 +0200

    fix: use floating-point perspective vectors

--- src/infrastructure/detection/opencv/normalizeCardPerspective.ts
+++ src/infrastructure/detection/opencv/normalizeCardPerspective.ts
@@ -5,8 +5,8 @@ import {
   InterpolationFlags,
   Mat,
   OpenCV,
-  Point,
-  PointVector,
+  Point2f,
+  Point2fVector,
   Scalar,
   Size,
 } from 'react-native-fast-opencv';
@@ -24,23 +24,23 @@ export const NORMALIZED_CARD_HEIGHT = 356;
 export function normalizeCardPerspective(source: Mat, corners: Quadrilateral): Mat {
   'worklet';
 
-  const sourcePoints = PointVector.create();
-  const destinationPoints = PointVector.create();
+  const sourcePoints = Point2fVector.create();
+  const destinationPoints = Point2fVector.create();
   const outputSize = Size.create(NORMALIZED_CARD_WIDTH, NORMALIZED_CARD_HEIGHT);
   const borderValue = Scalar.create(0, 0, 0, 0);
   const normalized = Mat.create(0, 0, DataTypes.CV_8U);
 
   const sourcePointObjects = [
-    Point.create(corners[0].x, corners[0].y),
-    Point.create(corners[1].x, corners[1].y),
-    Point.create(corners[2].x, corners[2].y),
-    Point.create(corners[3].x, corners[3].y),
+    Point2f.create(corners[0].x, corners[0].y),
+    Point2f.create(corners[1].x, corners[1].y),
+    Point2f.create(corners[2].x, corners[2].y),
+    Point2f.create(corners[3].x, corners[3].y),
   ] as const;
   const destinationPointObjects = [
-    Point.create(0, 0),
-    Point.create(NORMALIZED_CARD_WIDTH - 1, 0),
-    Point.create(NORMALIZED_CARD_WIDTH - 1, NORMALIZED_CARD_HEIGHT - 1),
-    Point.create(0, NORMALIZED_CARD_HEIGHT - 1),
+    Point2f.create(0, 0),
+    Point2f.create(NORMALIZED_CARD_WIDTH - 1, 0),
+    Point2f.create(NORMALIZED_CARD_WIDTH - 1, NORMALIZED_CARD_HEIGHT - 1),
+    Point2f.create(0, NORMALIZED_CARD_HEIGHT - 1),
   ] as const;
 
   let transform: Mat | null = null;
